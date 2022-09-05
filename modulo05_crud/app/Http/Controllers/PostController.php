3ae1831c5411c4a3ce548c632765754e0116e7f0
wrodriguess77 <willrodrigues4@gmail.com>
2022-09-05 08:19:03 -0300

    Create

--- modulo05_crud/app/Http/Controllers/PostController.php
+++ modulo05_crud/app/Http/Controllers/PostController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    public function create(Request $request)
+    {
+        // Definindo os valores do meu insert
+        $newPost = [
+            'title' => 'Meu primeiro post',
+            'content' => 'Conteúdo Qualquer',
+            'author' => 'Alessandro',
+        ];
+
+        // Instanciando o model/classe
+        $posts = new Post($newPost);
+
+        // Salvando efetivamente os dados no banco de dados
+        $posts->save();
+
+        return 'Post adicionado';
+
+        // OU
+        // $posts = new Post();
+        // $posts->title = 'Meu primeiro post';
+        // $posts->content = 'Conteúdo Qualquer';
+        // $posts->author = 'Alessandro';
+        // $posts->save();
+    }
 }
--- modulo05_crud/app/Models/Post.php
+++ modulo05_crud/app/Models/Post.php
@@ -7,5 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $fillable = [
+        'title',
+        'content',
+        'author'
+    ];
+
     use HasFactory;
 }
