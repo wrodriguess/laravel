174347c2a2ed1c32fc835c343b3f78aab922a7d9
wrodriguess77 <willrodrigues4@gmail.com>
2022-09-06 14:06:56 -0300

    Soft Delete

--- modulo05_crud/app/Models/Post.php
+++ modulo05_crud/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
@@ -12,6 +13,6 @@ class Post extends Model
         'content',
         'author'
     ];
-
+    use SoftDeletes;
     use HasFactory;
 }
