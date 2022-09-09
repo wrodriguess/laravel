9b5dee60e327941d87e3112bce97685e343013ea
wrodriguess77 <willrodrigues4@gmail.com>
2022-09-09 20:10:55 -0300

    Criando a estrutura da migrate

--- modulo06_migrations/database/migrations/2022_09_06_184759_create_products_table.php
+++ modulo06_migrations/database/migrations/2022_09_06_184759_create_products_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('code')->unique();
+            $table->integer('quantity')->default(0);
             $table->timestamps();
         });
     }
